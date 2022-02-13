1f2e5d36c8ad8c491d90ad009264df7f9dd428b2
Kitt Cheung <kitt.cheung@live.se>
2022-02-13 10:36:25 +0100

    FEAT: Added Classtype parameter to Armor. And Armor Stats. If-statement to check if Item is for Slot enum WEAPON. if it is, then item that Item can not be used in that slot.

--- Items/Armor.cs
+++ Items/Armor.cs
@@ -1,17 +1,23 @@
-﻿namespace RPGAssignment.Items
+﻿using RPGAssignment.Characters;
+using System;
+
+namespace RPGAssignment.Items
 {
     class Armor : Item
     {
+        public Stats Stats { get; set; }
 
         //public string Cloth { get; set; }
         //public string Leather { get; set; }
         //public string Mail { get; set; }
         //public string Plate { get; set; }
-
-        public Armor(string name, int reqLevel, Slot slot) : base(name, reqLevel, slot)
+        public Armor(string name, int reqLevel, Slot slot, ClassType classType, Stats stats) : base(name, reqLevel, slot, classType)
         {
-
+            Stats = stats;
+            if (Slot == Slot.WEAPON)
+            {
+                throw new ArgumentException("Armor can not be used in a Weapon slot.", nameof(slot));
+            }
         }
-
     }
 }
